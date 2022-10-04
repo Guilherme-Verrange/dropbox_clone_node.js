07d26c4c29e8b6f856b8a9b83fcf31210292abb1
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-10-04 09:39:47 -0300

    Added to function for create new folders.

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -1,6 +1,7 @@
 class DropBoxController {
   constructor() {
 
+    this.currentFolder = ['hcode'];
     this.onselectionchange = new Event('selectionchange');
     this.btnSendFileEl = document.querySelector("#btn-send-file");
     this.inputFilesEl = document.querySelector("#files");
@@ -61,6 +62,23 @@ class DropBoxController {
   }
 
   initEvents() {
+
+    this.btnNewFolder.addEventListener('click', e=> {
+
+      let name = prompt("Nome da nova pasta:");
+
+      if (name){
+
+        this.getFirebaseRef().push().set({
+          name,
+          type:'folder',
+          path:this.currentFolder.join('/')
+
+        });
+      }
+
+    });
+
     this.btnDelete.addEventListener("click", (e) => {
       this.removeTask().then((responses) => {
 
